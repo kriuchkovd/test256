89f3fc744efb10c8fde07726dcd371738d403aa3
Danylo <kriuchkovdanylo@gmail.com>
2022-02-16 16:27:10 +0200

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  mode: 'static',
+  router: {
+    base: '/your-github-repository-name/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-app',
